83561350e28333d4183d27874b8b7e4ed9067b6f
Behnaz1356 <behnaz_ghi@yahoo.com>
2016-11-20 23:57:59 -0800

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,4 +1,3 @@
-
 # Add your VPC ID to default below
 variable "vpc_id" {
   description = "VPC ID for usage throughout the build process"
@@ -202,3 +201,126 @@ resource "aws_route_table_association" "private_subnet_3_rt_assoc" {
     subnet_id = "${aws_subnet.private_subnet_3.id}"
     route_table_id = "${aws_route_table.pri_routing_table.id}"
 }
+#Create a DB subnet
+resource "aws_db_subnet_group" "DB_subnet_group" {
+    name = "DB subnet"
+    subnet_ids = ["${aws_subnet.private_subnet.id}", "${aws_subnet.private_subnet_2.id}"]
+    tags {
+        Name = "My DB subnet group"
+    }
+}
+#Ralation Database Service(RDS) Instances
+resource "aws_db_instance" "default" {
+  allocated_storage    = 5
+  engine               = "mariadb"
+  engine_version       = "10.0.24"
+  instance_class       = "db.t2.micro"
+  multi_az             = "NO"
+  name                 = "Maria Database"
+  username             = "master user"
+  password             = "${var.mariadb_password}"
+  storage type         = "gp2"
+  db_subnet_group_name = "${aws_db_subnet_group.DB_subnet_group.id}"
+   tags {
+        Name = "RDS Instances"
+    }
+
+#Create a security group 
+resource "aws_security_group" "sec_group" {
+  name = "sec_group"
+  description = "Allow sec_group inbound traffic"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "ISO Internet Protocol"
+      cidr_blocks = ["172.31.0.0/24"]
+  }
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "XEROX NS IDP"
+      cidr_blocks = ["172.31.0.0/24"]
+  }
+  tags {
+        Name = "Create new security group"
+    }
+
+}
+#Create a security group 
+resource "aws_security_group" "elb_group" {
+  name = "elb_group"
+  description = "Allow elb_group inbound traffic"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "ISO Internet Protocol"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags {
+        Name = "Create new security group"
+    }
+
+}
+
+# Create a new load balancer (ELB)
+resource "aws_elb" "elb" {
+  name = "elb"
+  subnets = "${aws_subnet.public_subnet_2.id}"
+  subnets = "${aws_subnet.public_subnet_3.id}"
+  security_groups = "${aws_security_group.elb_group.id}"
+  
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+    
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    target = "HTTP:80/"
+    interval = 30
+  }
+
+  instances = ["${aws_instance.foo.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout = 60
+  connection_draining = true
+  connection_draining_timeout = 60
+
+  tags {
+    Name = "elb"
+  }
+}
+#Create 2 instances that will run the web service
+
+resource "aws_instance" "webserver-b" {
+    ami = "ami-5ec1673e"
+    instance_type = "t2.micro"
+    private_ip = "${aws_subnet.private_subnet_2.id}"
+    key_name = "cit360"
+
+    tags {
+        Name = "webserver-b"
+        Service = "curriculum"
+    }
+  } 
+
+resource "aws_instance" "webserver-c" {
+    ami = "ami-5ec1673e"
+    instance_type = "t2.micro"
+    private_ip = "${aws_subnet.private_subnet_3.id}"
+    key_name ="cit360"
+
+    tags {
+        Name = "webserver-c"
+        Service = "curriculum"
+    }
+}
+  
